059e033665837be1b0b417b98d743ec0e89e0fe2
Nguyen Duc Duy <nguyenducduy10112004@gmail.com>
2025-02-12 22:24:23 +0700

    tối ưu tìm kiếm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -2,6 +2,7 @@
 
 const Product = require("../../models/product.model")
 const filterStatusHelpers = require("../../helpers/filterStatus")
+const searchProduct = require("../../helpers/search")
 module.exports.products = async (req, res) =>{
     //Đoạn bộ lọc
     const filterStatus = filterStatusHelpers(req.query);
@@ -9,12 +10,7 @@ module.exports.products = async (req, res) =>{
         deleted: false
     }
     //Tìm kiếm sản phẩm
-    let keyword = ""
-    if(req.query.keyword){
-        keyword = req.query.keyword
-        const regex = new RegExp(keyword, "i")
-        find.title = regex
-    }
+    const keyword = searchProduct(req.query,find)
     const products = await Product.find(find)
     res.render("admin/pages/products/index",{
         pageTitle: "Trang san pham",
---
+++ helpers/search.js
@@ -0,0 +1,9 @@
+module.exports = (query,find)=>{
+    let keyword = ""
+    if(query.keyword){
+        keyword = query.keyword
+        const regex = new RegExp(keyword, "i")
+        find.title = regex
+    }
+    return keyword
+}
