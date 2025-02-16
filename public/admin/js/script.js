f29e12c8d2e20f649fbb6ae52d32de9858e9151c
Nguyen Duc Duy <nguyenducduy10112004@gmail.com>
2025-02-16 21:03:11 +0700

    preview ảnh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -138,3 +138,18 @@ if(showAlert){
     })
 }
 //end show alert
+
+// upload img
+const uploadImage = document.querySelector("[upload-image]")
+if(uploadImage){
+    const uploadImageInput = document.querySelector("[upload-image-input]")
+    const uploadImagePreview = document.querySelector("[upload-image-preview]")
+    uploadImageInput.addEventListener("change", (e)=>{
+        const file = e.target.files[0]
+        console.log(file)
+        if(file){
+            uploadImagePreview.src = URL.createObjectURL(file)
+        }
+    })
+}
+// end upload img
